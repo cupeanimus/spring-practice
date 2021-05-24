35b444aa546910fd33e46370bcb2403018a3457f
kyle <cupeanimus@naver.com>
2021-05-24 17:14:18 +0900

    사용 db mysql -> h2 변경

--- src/test/java/com/kyle/springpractice/practice/jpa/StationRespoitoryTest.java
+++ src/test/java/com/kyle/springpractice/practice/jpa/StationRespoitoryTest.java
@@ -5,14 +5,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
 public class StationRespoitoryTest {
 
     @Autowired
